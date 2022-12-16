04df1d6809b9324b1c3c2f03482bb53e591a7194
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-12-16 00:05:27 -0300

    feat: create a simple kafka producer

---
+++ producer.js
@@ -0,0 +1,35 @@
+import { Kafka } from 'kafkajs';
+import { randomUUID } from 'node:crypto';
+
+async function bootstrap() {
+  const kafka = new Kafka({
+    brokers: ['********'],
+    sasl: {
+      mechanism: 'scram-sha-256',
+      username: '********',
+      password: '********',
+    },
+    ssl: true,
+  })
+
+  const producer = kafka.producer();
+
+  await producer.connect();
+  
+  await producer.send({
+    topic: 'notifications.send-notification',
+    messages: [
+      {
+        value: JSON.stringify({
+          content: 'Nova solicitação de amizade',
+          category: 'social',
+          recipientId: randomUUID(),
+        }),
+      },
+    ],
+  });
+
+  await producer.disconnect();
+}
+
+bootstrap();
